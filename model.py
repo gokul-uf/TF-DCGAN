e4f19e43cf900bf2e463c3f75c3822519f6ce76f
gokul <gokul.uf@gmail.com>
2017-10-11 10:18:11 +0200

    added loss computation implementation

--- model.py
+++ model.py
@@ -4,15 +4,11 @@ from __future__ import division
 
 import tensorflow as tf
 
+
 class DCGAN(object):
     """
         Tensorflow implementation of DCGAN, with four CNN layers.
         We assume the input images are of size 64x64. 
-
-        TODO
-            2. finish the compute_loss implementation
-            3. Figure out the gradient flow while the training process occurs, in maybe main.py ?
-            4. Create a pipeline class to feed data into the model
     """
 
     def __init__(self):
@@ -20,13 +16,16 @@ class DCGAN(object):
         self.noise_size = 100
         self.lrelu_alpha = 0.2
         self.num_channels = 3
+        self.lr = 0.0002
+        self.beta_1 = 0.5
         self._create_placeholders()
         self.generator_output = self._create_generator()
-        self.real_predictions = self._create_discriminator(
+        self.real_predictions, self.real_logits = self._create_discriminator(
             inputs=self.input_images)
-        self.fake_predictions = self._create_discriminator(
+        self.fake_predictions, self.fake_logits = self._create_discriminator(
             inputs=self.generator_output, reuse=True)
         self._compute_loss()
+        self.summary_op = tf.summary.merge_all()
 
     def _create_placeholders(self):
         self.input_images = tf.placeholder(
@@ -34,7 +33,9 @@ class DCGAN(object):
             dtype=tf.float32,
             name="input_images")
         self.input_noise = tf.placeholder(
-            shape=[None, self.noise_size], dtype=tf.float32, name="input_noise")
+            shape=[None, self.noise_size],
+            dtype=tf.float32,
+            name="input_noise")
 
     def _create_generator(self):
         xav_init = tf.contrib.layers.xavier_initializer
@@ -46,7 +47,7 @@ class DCGAN(object):
                 self.input_noise,
                 shape=[tf.shape(fc_1)[0], 4, 4, 512],
                 name="reshaped_noise")
-            print("reshaped_noise shape: {}".format(reshaped_noise.shape))
+
             def _create_deconv_bnorm_block(inputs,
                                            name,
                                            filters,
@@ -66,32 +67,27 @@ class DCGAN(object):
 
             bnorm_1 = _create_deconv_bnorm_block(
                 inputs=reshaped_noise, filters=256, name="block_1")
-            print("bnorm_1 shape: {}".format(bnorm_1.shape))
 
             bnorm_2 = _create_deconv_bnorm_block(
                 inputs=bnorm_1, filters=128, name="block_2")
-            print("bnorm_2 shape: {}".format(bnorm_2.shape))
 
             bnorm_3 = _create_deconv_bnorm_block(
                 inputs=bnorm_2, filters=64, name="block_3")
-            print("bnorm_3 shape: {}".format(bnorm_3.shape))
 
             bnorm_4 = _create_deconv_bnorm_block(
                 inputs=bnorm_3,
                 filters=3,
                 activation=tf.nn.tanh,
                 name="block_4")
-            print("bnorm_4 shape: {}".format(bnorm_4.shape))
+
             return bnorm_4
 
-    def _create_discriminator(
-            self,
-            inputs, reuse=False):  #  TODO(gokuls) need to figure out how to share weights
+    def _create_discriminator(self, inputs, reuse=False):
         xav_init = tf.contrib.layers.xavier_initializer
         bnorm = tf.layers.batch_normalization
         with tf.variable_scope("discriminator", reuse=reuse):
 
-            def _create_conv_bnorm_block(inputs, name, filters):
+            def _create_conv_bnorm_block(inputs, filters, name):
                 with tf.variable_scope(name, reuse=reuse):
                     conv = tf.layers.conv2d(
                         inputs=inputs,
@@ -115,29 +111,49 @@ class DCGAN(object):
                 padding="same",
                 name="conv_1")
             conv_1 = tf.maximum(conv_1, self.lrelu_alpha * conv_1)
-            print("conv_1 shape: {}".format(conv_1.shape))
 
             bnorm_1 = _create_conv_bnorm_block(
                 inputs=conv_1, filters=128, name="block_1")
-            print("bnorm_1 shape: {}".format(bnorm_1.shape))
 
             bnorm_2 = _create_conv_bnorm_block(
                 inputs=bnorm_1, filters=256, name="block_2")
-            print("bnorm_2 shape: {}".format(bnorm_2.shape))
 
             bnorm_3 = _create_conv_bnorm_block(
                 inputs=bnorm_2, filters=512, name="block_3")
-            print("bnorm_3 shape: {}".format(bnorm_3.shape))
 
             reshaped_bnorm_3 = tf.reshape(
                 bnorm_3,
                 shape=[tf.shape(bnorm_3)[0], 4 * 4 * 512],
                 name="reshaped_bnorm_3")
 
-            fc_1 = tf.layers.dense(
-                inputs=reshaped_bnorm_3, units=1, name="fc_1")
-            print("fc_1 shape: {}".format(fc_1.shape))
-            return fc_1
+            logits = tf.layers.dense(
+                inputs=reshaped_bnorm_3,
+                units=1,
+                name="fc_1")
+            fc_1 = tf.sigmoid(logits)
+            return fc_1, logits
 
     def _compute_loss(self):
-        pass
+        self.d_loss_real = tf.nn.sigmoid_cross_entropy_with_logits(logits = self.real_logits, labels = tf.ones_like(self.real_logits))
+        self.d_loss_real = tf.reduce_mean(self.d_loss_real)
+        
+        self.d_loss_fake = tf.nn.sigmoid_cross_entropy_with_logits(logits = self.fake_logits, labels = tf.zeros_like(self.fake_logits))
+        self.d_loss_fake = tf.reduce_mean(self.d_loss_fake)
+
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+        self.g_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits = self.fake_logits, labels = tf.ones_like(self.fake_logits))
+        self.g_loss = tf.reduce_mean(self.g_loss)
+        
+        tf.summary.scalar("disc_loss_real", self.d_loss_real)
+        tf.summary.scalar("disc_loss_fake", self.d_loss_fake)
+        tf.summary.scalar("disc_loss", self.d_loss)
+        tf.summary.scalar("gen_loss", self.g_loss)
+
+        d_opt = tf.train.AdamOptimizer(learning_rate = self.lr, beta1 = self.beta_1)
+        g_opt = tf.train.AdamOptimizer(learning_rate = self.lr, beta1 = self.beta_1)
+        
+        d_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="discriminator")
+        g_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="generator")
+
+        self.d_train = d_opt.minimize(self.d_loss, var_list = d_vars)
+        self.g_train = g_opt.minimize(self.g_loss, var_list = g_vars) 
